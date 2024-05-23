dada2265ccbaa80aa7730343c974d9306d1b7131
VinnyTheLegend <66708167+VinnyTheLegend@users.noreply.github.com>
2024-05-22 22:45:21 -0400

    yeah

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,42 +1,44 @@
 <script lang="ts">
   import NavBar from "./components/NavBar.svelte";
-  
-  const BASE_URL: string = "https://localhost:8000"
+
+  const BASE_URL: string = "https://localhost:8000";
 
   const URLS = {
     BASE_URL: BASE_URL,
     AUTH_URL: BASE_URL + "/discord/authenticate",
     ME_URL: BASE_URL + "/discord/me",
-    URL_PARAMS: new URLSearchParams(window.location.search)
-  }
+    URL_PARAMS: new URLSearchParams(window.location.search),
+  };
 
-  const ROLES_DICT: { [id: string] : string; } = {
+  const ROLES_DICT: { [id: string]: string } = {
     "591686220996935691": "Warlord",
     "591686523142012948": "General",
     "591687458819932172": "Veteran",
-    "591687038902992928": "Member"
-  }
+    "591687038902992928": "Member",
+  };
 
-  let USER_INFO: UserData | null = null
+  let USER_INFO: UserData | null = null;
 
   async function userUpdate() {
-    fetch(URLS.ME_URL, {mode: 'cors', credentials: 'include'})
+    fetch(URLS.ME_URL, { mode: "cors", credentials: "include" })
       .then((response) => {
         if (response.status === 400) {
           return response.json().then((data) => {
-            throw new Error(data.detail || 'Bad request')
-          })
+            throw new Error(data.detail || "Bad request");
+          });
         }
         return response.json();
       })
       .then((data) => {
-        if ('error' in data) { console.log(data) } else {
-          console.log(data)
-          USER_INFO = { ...data }
+        if ("error" in data) {
+          console.log(data);
+        } else {
+          console.log(data);
+          USER_INFO = { ...data };
         }
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
         // AuthRedirect()
         return [];
       });
@@ -44,22 +46,20 @@
 
   function echo(message: string) {
     fetch(BASE_URL + "/api/echo", {
-      mode: 'cors', 
-      credentials: 'include',
+      mode: "cors",
+      credentials: "include",
       method: "POST",
-      headers: {'Content-Type': 'application/json'}, 
-      body: JSON.stringify({message: message})
-    }).then(res => {
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ message: message }),
+    }).then((res) => {
       console.log("Echo response", res);
     });
   }
-
 </script>
 
 <main>
-  <NavBar {USER_INFO} {URLS} {ROLES_DICT} on:userUpdate={userUpdate} echo={echo}/>
+  <NavBar {USER_INFO} {URLS} {ROLES_DICT} on:userUpdate={userUpdate} {echo} />
 </main>
 
 <style>
-
 </style>
--- front/src/components/NavBar.svelte
+++ front/src/components/NavBar.svelte
@@ -1,65 +1,67 @@
 <script lang="ts">
-    import { createEventDispatcher } from 'svelte'
-    const dispatch = createEventDispatcher()
-  
-    export let ROLES_DICT
-  
-    export let USER_INFO: UserData | null
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
 
-    export let URLS: any
-  
+    export let ROLES_DICT;
+
+    export let USER_INFO: UserData | null;
+
+    export let URLS: any;
+
     function userUpdate() {
-        dispatch('userUpdate')
+        dispatch("userUpdate");
     }
-    
-    export let echo: Function
+
+    export let echo: Function;
 
     function echoSubmit(e: SubmitEvent) {
-        const target = e.target as HTMLFormElement
-        const formData = new FormData(target)
-        const message = formData.get("message")
-        target.reset()
-        echo(message)
+        const target = e.target as HTMLFormElement;
+        const formData = new FormData(target);
+        const message = formData.get("message");
+        target.reset();
+        echo(message);
     }
+</script>
 
-  </script>
-  
-  <main>
+<main>
     <div class="flex items-center">
-      <button on:click={() => {window.location.href = URLS.AUTH_URL}}> Authenticate </button>
-      <button on:click={userUpdate}> Discord Info </button>
-      {#if USER_INFO}
-      
-        <div class="flex p-2">
-          <h1>Name: {USER_INFO.nickname}</h1>
-          <h1>Admin: {USER_INFO.admin}</h1>
-        </div>
-  
         <button
-        on:click={echo('test')}
-      >
-        test
-      </button>
-  
-        <ul>
-          {#if USER_INFO.roles}
-            {#each USER_INFO.roles as role}
-             <span class="p-2 m-2 border-2 border-purple-500">{ROLES_DICT[role]}</span>
-            {/each}
-          {/if}
-        </ul>
-  
-        <form action="" on:submit|preventDefault={echoSubmit} class="flex items-center">
-          <input type="text" name="message" class="h-5 text-black">
-          <button class="ml-2">Send</button>
-        </form>
-  
-      {/if}
+            on:click={() => {
+                window.location.href = URLS.AUTH_URL;
+            }}
+        >
+            Authenticate
+        </button>
+        <button on:click={userUpdate}> Discord Info </button>
+        {#if USER_INFO}
+            <div class="flex p-2">
+                <h1>Name: {USER_INFO.nickname}</h1>
+                <h1>Admin: {USER_INFO.admin}</h1>
+            </div>
+
+            <button on:click={echo("test")}> test </button>
+
+            <ul class="flex justify-around px-5">
+                {#if USER_INFO.roles}
+                    {#each USER_INFO.roles as role}
+                        <span class="p-2 border-2 border-purple-500"
+                            >{ROLES_DICT[role]}</span
+                        >
+                    {/each}
+                {/if}
+            </ul>
+
+            <form
+                action=""
+                on:submit|preventDefault={echoSubmit}
+                class="flex items-center"
+            >
+                <input type="text" name="message" class="h-5 text-black" />
+                <button class="ml-2">Send</button>
+            </form>
+        {/if}
     </div>
-  
-  </main>
-  
-  <style>
-  
-  </style>
-  
+</main>
+
+<style>
+</style>
