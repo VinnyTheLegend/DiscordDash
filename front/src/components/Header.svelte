c791ee52e0db9a6c2bd2f9b5a446b301502ed324
VinnyTheLegend <66708167+VinnyTheLegend@users.noreply.github.com>
2024-06-19 23:01:54 -0400

    clean up front

--- front/src/components/Header.svelte
+++ front/src/components/Header.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
-    import { echo, URLS, ROLES_DICT } from "../utils";
     import sedicon from "../assets/sedicon.webp"
 
     export let USER: User;
-
-    function echoSubmit(e: SubmitEvent) {
-        const target = e.target as HTMLFormElement;
-        const formData = new FormData(target);
-        const message = formData.get("message");
-        target.reset();
-        if (typeof message === "string" && message != "") {
-            echo(message);
-        }
-    }
     
     function getAvatarUrl(id: bigint, avatar: string): string {
         let url = `https://cdn.discordapp.com/avatars/${id}/${avatar}.png`
@@ -22,9 +11,9 @@
 
 <main class="border-b-[1px] border-[#47003C] h-[10%]">
     <div class="flex items-center h-full w-full justify-between px-4">
-        <div class="w-[11%]"><img src="{sedicon}" alt="" class="size-14 rounded-full"></div>
+        <div class="w-[25%]"><img src="{sedicon}" alt="" class="size-14 rounded-full"></div>
         <header class="seduction font-extrabold text-5xl">SEDUCTION</header>
-        <div class="flex items-center justify-center w-[11%]">
+        <div class="flex items-center justify-end w-[25%]">
             <span class="pr-4 font-bold">{USER.username}</span>
             <img src="{getAvatarUrl(USER.id, USER.avatar)}" alt="" class="size-14 rounded-full">
         </div>
---
+++ front/src/components/SideBar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let USER: User;
+
+</script>
+
+<main class="">
+</main>
+
+<style>
+
+</style>
