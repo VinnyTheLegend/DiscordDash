da74ce088b37449e90c16cc20b8c03c1a39dd599
VinnyTheLegend <66708167+VinnyTheLegend@users.noreply.github.com>
2024-06-23 21:52:03 -0400

    clean up

--- front/src/components/Header.svelte
+++ front/src/components/Header.svelte
@@ -7,7 +7,6 @@
     import { Label } from "$lib/components/ui/label/index.js";
     import { Button } from "$lib/components/ui/button";
 
-
     import {URLS} from "../utils"
 
     export let USER: User;
@@ -36,45 +35,45 @@
         <div class="flex items-center justify-end w-[25%]">
             <span class="pr-4 font-bold">{USER.username}</span>
             <Dialog.Root>
-            <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                    <Avatar.Root>
-                        <Avatar.Image src={`https://cdn.discordapp.com/avatars/${USER.id}/${USER.avatar}.png`} alt="" />
-                        <Avatar.Fallback>{USER.username[0].toUpperCase()}</Avatar.Fallback>
-                    </Avatar.Root>
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content class="">
-                  <DropdownMenu.Group>
-                    <DropdownMenu.Label>My Account</DropdownMenu.Label>
-                    <DropdownMenu.Separator />
-                    <Dialog.Trigger><DropdownMenu.Item>Role Selection</DropdownMenu.Item></Dialog.Trigger>
-                    <DropdownMenu.Item class="bg-destructive data-[highlighted]:bg-red-700" on:click={() => window.location.href=`${URLS.BASE_URL}/logout`}>Sign Out</DropdownMenu.Item>
-                  </DropdownMenu.Group>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
+                <DropdownMenu.Root>
+                    <DropdownMenu.Trigger>
+                        <Avatar.Root>
+                            <Avatar.Image src={`https://cdn.discordapp.com/avatars/${USER.id}/${USER.avatar}.png`} alt="" />
+                            <Avatar.Fallback>{USER.username[0].toUpperCase()}</Avatar.Fallback>
+                        </Avatar.Root>
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content class="">
+                        <DropdownMenu.Group>
+                            <DropdownMenu.Label>My Account</DropdownMenu.Label>
+                            <DropdownMenu.Separator />
+                            <Dialog.Trigger><DropdownMenu.Item>Role Selection</DropdownMenu.Item></Dialog.Trigger>
+                            <DropdownMenu.Item class="bg-destructive data-[highlighted]:bg-red-700" on:click={() => window.location.href=`${URLS.BASE_URL}/logout`}>Sign Out</DropdownMenu.Item>
+                        </DropdownMenu.Group>
+                    </DropdownMenu.Content>
+                    </DropdownMenu.Root>
                 <Dialog.Content>
-                  <Dialog.Header>
-                    <Dialog.Title class="text-center">Role Selection</Dialog.Title>
-                  </Dialog.Header>
-                  <div class="grid grid-cols-3">
-                    {#each optional_roles as role}
-                        <div class="flex items-center space-x-2 w-auto mx-4 my-2">
-                            <Checkbox id="terms" bind:checked={role.checked} onCheckedChange={() => roleChange(role)} aria-labelledby="terms-label" />
-                            <Label
-                              id="terms-label"
-                              for="terms"
-                              class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-center"
-                            >
-                              {role.name}
-                            </Label>        
-                        </div>
-                    {/each}
-                  </div>
-                  <Dialog.Footer>
-                    <Button type="submit">Save changes</Button>
-                  </Dialog.Footer>
+                    <Dialog.Header>
+                        <Dialog.Title class="text-center">Role Selection</Dialog.Title>
+                    </Dialog.Header>
+                    <div class="grid grid-cols-3">
+                        {#each optional_roles as role}
+                            <div class="flex items-center space-x-2 w-auto mx-4 my-2">
+                                <Checkbox id="terms" bind:checked={role.checked} onCheckedChange={() => roleChange(role)} aria-labelledby="terms-label" />
+                                <Label
+                                id="terms-label"
+                                for="terms"
+                                class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-center"
+                                >
+                                {role.name}
+                                </Label>        
+                            </div>
+                        {/each}
+                    </div>
+                    <Dialog.Footer>
+                        <Button type="submit">Save changes</Button>
+                    </Dialog.Footer>
                 </Dialog.Content>
-              </Dialog.Root>
+            </Dialog.Root>
         </div>
     </div>
 </main>
