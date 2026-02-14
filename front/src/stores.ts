5de24ee1430f6f3386470b696c8f36d5c4477420
VinnyTheLegend <sometimesvinny@gmail.com>
2026-02-14 23:00:09 +0000

    reset release to main

--- front/src/stores.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-import type { Writable } from 'svelte/store';
-
-export const members: Writable<UserData[]> = writable([]);
-export const guild_info: Writable<GuildInfo> = writable();
